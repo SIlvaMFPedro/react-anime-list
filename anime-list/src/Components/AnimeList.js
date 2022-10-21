118aa6df0b3746753468b8f899333328d96071c4
silvamfpedro <silva.mfpedro@gmail.com>
2022-10-21 16:30:37 +0100

    Added changes in AnimeList component

--- anime-list/src/Components/AnimeList.js
+++ anime-list/src/Components/AnimeList.js
@@ -3,9 +3,61 @@ import { Image, ActivityIndicator, Text, View, Dimensions, TouchableOpacity} fro
 import { AnimeContext } from "../API/context";
 import Carousel from "react-native-snap-carousel";
 
-const AnimeList = () => {
+const windowWidth = Dimensions.get("window").width;
+const SLIDE_WIDTH = Math.round(windowWidth / 3.5);
+
+const AnimeList = ({ id, setTarget, setVisible }) => {
+
+    const { fetchAnimeCategories, darkTheme, setError } = useContext(AnimeContext);
+    const [Loading, setLoading] = useState(true);
+    const [GenreName, setGenreName] = useState('');
+    const [GenreList, setGenreList] = useState([]);
+
+    useEffect(
+        () => {
+            fetchAnimeCategories(id).then(data => {
+                setGenreList(data.anime)
+                setGenreName(data.mal_url.name)
+                setLoading(false)
+            }).catch(e => setError(e))
+        }, [fetchAnimeCategories, id, setError]
+    )
+    
     return (
-        <div></div>
+        <View style={{ backgroundColor: darkTheme ? '#282C35' : '#f2f2f2', marginTop: 20 }} horizontal key={id}>
+            <Text style={{
+                fontSize: 20,
+                color: darkTheme ? '#E4FBFF' : 'black',
+                fontWeight: 'bold',
+                textAlign: 'left',
+                padding: 10
+            }}>{GenreName}</Text>
+            {
+                Loading ? <ActivityIndicator size='large' color='#7868E6'/> : (
+                    <Carousel 
+                        layout={"default"}
+                        data={GenreList}
+                        renderItem={({ item, index }) => {
+                            return (
+                                <TouchableOpacity onPress={() => {
+                                    setTarget(item)
+                                    setVisible(true)
+                                }} key={index}>
+                                    <Image source={{ url: item.image_url }} style={{ height:100, width:100, margin:10 }}/>
+                                    <Text numberOfLines={2} 
+                                          style={{ color: darkTheme ? '#fff' : 'black', maxWidth: 100, textAlign: 'center'}}>{item.title}</Text>
+                                </TouchableOpacity>
+                            );
+                        }}
+                        sliderWidth={windowWidth}
+                        itemWidth={SLIDE_WIDTH}
+                        activeSlideAlignment={"start"}
+                        inactiveSlideScale={1}
+                        inactiveSlideOpacity={1}
+                    />
+                )
+                    }
+        </View>
     );
 }
 
